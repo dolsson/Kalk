afa735203024b5f4079e8b883d38738daa35b724
dolsson <grog.25@hotmail.com>
2013-11-15 01:48:12 +0100

    First kalk

---
+++ Kalk.cpp
@@ -0,0 +1,19 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int a;
+    int b;
+    int sum;
+    cout<<"Skriv in ett tal\n "<<endl;
+    cin>>a;
+    cout<<"\n+\n"<<endl;
+    cin>>b;
+    sum=a+b;
+    cout<<"\nSumman = "<<sum<<endl;
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
