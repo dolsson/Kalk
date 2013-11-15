f033982343be61a3f2e503921f1f9bf101a0de62
dolsson <grog.25@hotmail.com>
2013-11-15 02:00:09 +0100

    Negativ
    La till en negativ function

--- Kalk.cpp
+++ Kalk.cpp
@@ -7,13 +7,16 @@ int main(int argc, char *argv[])
 {
     int a;
     int b;
+    int nsum;
     int sum;
     cout<<"Skriv in ett tal\n "<<endl;
     cin>>a;
     cout<<"\n+\n"<<endl;
     cin>>b;
     sum=a+b;
+    nsum=a-b;
     cout<<"\nSumman = "<<sum<<endl;
+    cout<<"\nSumman = "<<nsum<<endl;
     system("PAUSE");
     return EXIT_SUCCESS;
 }
