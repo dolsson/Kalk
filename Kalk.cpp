26ef6f609ebf558e297e1656530cca0608a42ceb
alelar <alex.lar@hotmail.com>
2013-11-18 13:50:03 +0100

    Update Kalk.cpp
    gånger

--- Kalk.cpp
+++ Kalk.cpp
@@ -9,14 +9,17 @@ int main(int argc, char *argv[])
     int b;
     int nsum;
     int sum;
+    int gsum;
     cout<<"Skriv in ett tal\n "<<endl;
     cin>>a;
     cout<<"\n+\n"<<endl;
     cin>>b;
     sum=a+b;
     nsum=a-b;
+    gsum=a*b;
     cout<<"\nSumman = "<<sum<<endl;
     cout<<"\nSumman = "<<nsum<<endl;
+    cout<<"\nSumman = "<<gsum<<endl;
     system("PAUSE");
     return EXIT_SUCCESS;
 }
